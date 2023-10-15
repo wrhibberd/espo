5d4f35baecacc01999518ee598708e17fcaa78d2
William Hibberd <will@williamhibberd.co.uk>
2023-10-15 11:32:13 +0100

    add staging site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://lighthearted-sopapillas-24f39e.netlify.app/",
 	integrations: [sitemap()],
 });
