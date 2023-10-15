5779ea3af9127f845b8fece87ad097030018ec29
William Hibberd <will@williamhibberd.co.uk>
2023-10-15 11:05:30 +0100

    remove tailwind, add basic styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
 import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-		sitemap(),
-	],
+	integrations: [sitemap()],
 });
