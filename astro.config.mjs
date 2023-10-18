a3a7f776b403377de286499f5cfea97b2fad11f1
William Hibberd <will@williamhibberd.co.uk>
2023-10-18 15:14:23 +0100

    re-instate tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://lighthearted-sopapillas-24f39e.netlify.app/",
-	integrations: [sitemap()],
+	integrations: [
+		sitemap(),
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+	],
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		colors: {
+			transparent: "transparent",
+			current: "currentColor",
+			black: "#000",
+			white: "#fff",
+			green: "#001711",
+		},
+		extend: {},
+	},
+	plugins: [],
+};
