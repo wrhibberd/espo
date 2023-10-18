fb1d2cce0df2d61db462088d2a25eb7c2c7ad624
William Hibberd <will@williamhibberd.co.uk>
2023-10-18 16:10:45 +0100

    added fonts, view transition, images, and styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
+		fontFamily: {
+			serif: ['"brick"', ...defaultTheme.fontFamily.serif],
+			sans: ['"Inter"', ...defaultTheme.fontFamily.serif],
+		},
 		colors: {
 			transparent: "transparent",
 			current: "currentColor",
 			black: "#000",
 			white: "#fff",
 			green: "#001711",
+		},
+		fontSize: {
+			sm: ["0.6875rem", "1.18em"],
 		},
 		extend: {},
 	},
